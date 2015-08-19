83f25804e2ee6dd3028060616d85519f2ba19e5e
gixxy <gus3@michelfamily.org>
2015-08-19 16:16:40 -0500

    Rewrite PA JS

--- files/photo_album.js
+++ files/photo_album.js
@@ -1,3 +1,4 @@
+/*
 var next = jQuery.Event("keydown");
 next.which = 39; // # Some key code value
 $(".btn-next").trigger(next);
@@ -5,3 +6,15 @@ $(".btn-next").trigger(next);
 var previous = jQuery.Event("keydown");
 previous.which = 37; // # Some key code value
 $(".btn-previous").trigger(previous);
+*/
+
+var previous = $('.btn-previous')[0],
+    next = $('.btn-next')[0];
+
+$(window).keydown(function (e) {
+    if ( e.which === 37 ) {
+        window.location.href = previous.href;
+    } else if ( e.which === 39 ) {
+        window.location.href = next.href;
+    }
+});
